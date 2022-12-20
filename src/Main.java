6b6752e3c160356ca137a54c01fffce02f99f6bc
mihankalani <mihan.kalani@gmail.com>
2022-12-19 21:15:01 -0500

    the feature have been added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        CarFeature xl=new CarFeature();
-        System.out.println("Hello world!");
+        CarFeature xl = new CarFeature();
+        System.out.println("launch the car");
+        xl.color = "red";
+        xl.doors = "5";
+        xl.engine = 32323;
+        xl.model = "a5";
     }
 }
