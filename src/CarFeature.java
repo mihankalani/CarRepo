912ed539a8fd7db954fcaf9bf361b154c9347d83
mihankalani <mihan.kalani@gmail.com>
2022-12-19 20:52:41 -0500

    it's requirment for lunching car

---
+++ src/CarFeature.java
@@ -0,0 +1,7 @@
+public class CarFeature {
+        int engine;
+        String model;
+        String color;
+        String doors;
+
+    }
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        CarFeature xl=new CarFeature();
+        System.out.println("Hello world!");
+    }
+}
